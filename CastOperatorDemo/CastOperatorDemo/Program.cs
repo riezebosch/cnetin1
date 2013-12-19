b6b1f6b019eb37e5ef7ba0619daa2f2e877be040
Manuel Riezebosch <mriezebosch@gmail.com>
2013-12-19 11:57:40 +0100

    Cast operator demo

---
+++ CastOperatorDemo/CastOperatorDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CastOperatorDemo
+{
+    class Program : string
+    {
+        class BankAccount
+        {
+            public string Number { get; set; }
+
+            public static implicit operator BankAccount(string input)
+            {
+                return new BankAccount { Number = input };
+            }
+
+            public static BankAccount operator +(BankAccount account, string input)
+            {
+                account.Number += input;
+                return account;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount { Number = "123456789" };
+            BankAccount ba2 = "123456789";
+
+            // Deze is lelijk omdat de + operator ook de oorspronkelijke instantie aanpast.
+            ba = ba + "nog een string";
+
+            // Voorbeeld waar toegepast in het .NET framework, druk op F12 om naar
+            // de type definitie te gaan.
+            XNamespace xmlns = "urn:www-infosupport-com:training:cnetin";
+
+
+            int i2 = int.Parse("2");
+            int i3 = Convert.ToInt32("3");
+        }
+    }
+}
